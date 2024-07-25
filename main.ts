1af68fc76cc25bcd4fd4af315034b8f1da245da5
bennyyip <yebenmy@protonmail.com>
2024-07-25 14:44:56 +0800

    init

--- main.ts
+++ main.ts
@@ -1,134 +1,186 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  Editor,
+  MarkdownView,
+  Modal,
+  Notice,
+  parseLinktext,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  Workspace,
+} from 'obsidian'
+import type { MarkdownEditView } from 'obsidian'
+import { openInMpv, isVideoLink, isVideoExt, getInstancePrototype, getRunningMarkdownViewInstance } from './utils'
+import type { PreviewEventHanlder } from 'obsidian'
+import { MarkdownPreviewRenderer } from 'obsidian'
+import { around } from 'monkey-around'
+import * as path from 'path'
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface MpvPluginSettings {
+  mpvPath: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: MpvPluginSettings = {
+  mpvPath: 'mpv',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MpvPlugin extends Plugin {
+  settings: MpvPluginSettings
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings()
+    this.loadPatches()
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  onunload() {}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  async saveSettings() {
+    await this.saveData(this.settings)
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+  private loadPatches() {
+    this.patchPreviewClick()
+    this.patchEditorClick()
+    this.patchLinktextOpen()
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  private patchEditorClick() {
+    const plugin = this
+    getRunningMarkdownViewInstance(plugin).then((view) => {
+      if (!view.editMode) {
+        console.error('MarkdownView.editMode is not available, cannot patch editor click')
+        return
+      }
+      plugin.register(
+        around(getInstancePrototype(view.editMode), {
+          triggerClickableToken: (next) =>
+            async function (this: MarkdownEditView, token, newLeaf, ...args) {
+              const fallback = () => next.call(this, token, newLeaf, ...args)
+              const link = token.text
+              console.debug('patchEditorClick', link)
+              if (!(isVideoLink(link) && 'external-link' === token.type)) {
+                return fallback()
+              }
+              try {
+                await openInMpv(link)
+              } catch (e) {
+                console.error(`onExternalLinkClick error in editor, fallback to default`, e)
+                return fallback()
+              }
+            },
+        }),
+      )
+      console.debug('editor click patched')
+    })
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  private patchPreviewClick() {
+    const plugin = this
+    function patchPreviewEventHanlder(handler: PreviewEventHanlder, plugin: MpvPlugin) {
+      plugin.register(
+        around(getInstancePrototype(handler), {
+          onExternalLinkClick: (next) =>
+            async function (this: PreviewEventHanlder, evt, target, link, ...args) {
+              const fallback = () => next.call(this, evt, target, link, ...args)
+              console.debug('patchPreviewClick', link)
+              if (!isVideoLink(link)) {
+                return fallback()
+              }
+              evt.preventDefault()
+              try {
+                await openInMpv(link)
+              } catch (e) {
+                console.error(`onExternalLinkClick error in preview, fallback to default`, e)
+                return fallback()
+              }
+            },
+        }),
+      )
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    const unloadPatchHook = around(MarkdownPreviewRenderer as MDPreviewRendererCtor, {
+      registerDomEvents: (next) =>
+        function (this: MarkdownPreviewRenderer, _el, helper, ...args) {
+          patchPreviewEventHanlder(helper, plugin)
+          unloadPatchHook()
+          console.debug('preview click patched')
+          return next.call(this, _el, helper, ...args)
+        },
+    })
+  }
 
-	onunload() {
+  private patchLinktextOpen() {
+    const plugin = this
+    this.register(
+      around(Workspace.prototype, {
+        openLinkText: (next) =>
+          async function (this: Workspace, linktext, sourcePath, newLeaf, openViewState, ...args) {
+            const fallback = () => next.call(this, linktext, sourcePath, newLeaf, openViewState, ...args)
+            console.debug('patchLinktextOpen', linktext, sourcePath)
+            try {
+              await plugin.openInternalLink(linktext, sourcePath, fallback)
+            } catch (e) {
+              console.error(`onInternalLinkClick error in openLinktext, fallback to default`, e)
+              fallback()
+            }
+          },
+      }),
+    )
+    console.debug('linktext open patched')
+  }
 
-	}
+  private async openInternalLink(linktext, sourcePath, fallback) {
+    const { metadataCache } = this.app
+    const basePath = (this.app.vault.adapter as any).basePath
+    const { path: linkpath, subpath } = parseLinktext(linktext)
+    const linkFile = metadataCache.getFirstLinkpathDest(linkpath, sourcePath)
+    if (!linkFile || !isVideoExt(linkFile.extension)) {
+      fallback()
+      return
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    const absolutePath = path.posix.join(basePath, linkFile.path)
+    console.debug('openInternalLink', absolutePath)
+    if (subpath.startsWith('#t=')) {
+      const timestamp = subpath.slice(3)
+      await openInMpv(absolutePath, timestamp)
+    } else {
+      await openInMpv(absolutePath)
+    }
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+declare module 'obsidian' {
+  interface MarkdownEditView {
+    triggerClickableToken(
+      token: { type: string; text: string; start: number; end: number },
+      newLeaf: boolean | PaneType,
+    ): void
+  }
+  interface MarkdownView {
+    // for safe access
+    editMode?: MarkdownEditView
+  }
+  class PreviewEventHanlder {
+    app: App
+    onInternalLinkDrag(evt: MouseEvent, delegateTarget: HTMLElement, linktext: string): void
+    onInternalLinkClick(evt: MouseEvent, delegateTarget: HTMLElement, linktext: string): void
+    onInternalLinkRightClick(evt: MouseEvent, delegateTarget: HTMLElement, linktext: string): void
+    onExternalLinkClick(evt: MouseEvent, delegateTarget: HTMLElement, href: string): void
+    onInternalLinkMouseover(evt: MouseEvent, delegateTarget: HTMLElement, href: string): void
+    onTagClick(evt: MouseEvent, delegateTarget: HTMLElement, tag: string): void
+    info?: MarkdownView | MarkdownFileInfo
+  }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+type MDPreviewRendererCtor = typeof MarkdownPreviewRenderer & {
+  registerDomEvents(el: HTMLElement, helper: PreviewEventHanlder, isBelongTo: (el: HTMLElement) => boolean): void
+  belongsToMe(target: HTMLElement, el: HTMLElement, isBelongTo: (el: HTMLElement) => boolean): boolean
 }
---
+++ utils.ts
@@ -0,0 +1,178 @@
+import type { App, MarkdownView, Plugin, View, Workspace } from 'obsidian'
+import { spawn } from 'child_process'
+import * as net from 'net'
+import { WorkspaceLeaf } from 'obsidian'
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+export async function openInMpv(link: string, timestamp: string | null = null) {
+  console.debug(`opening ${link} ${timestamp}`)
+  const pipePath = getPipePath(link)
+  console.debug(pipePath)
+  const alreadyOpen = await PipeExists(pipePath)
+  if (!alreadyOpen) {
+    const args = [link, `--input-ipc-server=${pipePath}`]
+    if (timestamp !== null) {
+      args.push(`--start=${timestamp}`)
+    }
+    const subprocess = spawn('mpv', args, {
+      detached: true,
+      stdio: 'ignore',
+    })
+    if (typeof subprocess.pid !== 'number') {
+      console.error('failed to spawn mpv')
+      return
+    }
+    subprocess.unref()
+  }
+  if (timestamp !== null) {
+    const nRetries = 10
+    for (let i = 0; i < nRetries; i++) {
+      if (await PipeExists(pipePath)) {
+        break
+      }
+      await sleep(10)
+    }
+    mpvSeek(pipePath, timestamp)
+  }
+  // subprocess.on('error', (error) => {
+  //   console.error('open in mpv failed', error)
+  // })
+  // subprocess.stderr?.on('data', (data) => console.error('open in mpv stderr:', data.toString()))
+  // subprocess.stdout?.on('data', (data) => console.error('open in mpv stdout:', data.toString()))
+}
+
+function PipeExists(pipePath): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    const socket = net.connect(pipePath)
+    socket.on('error', (err) => {
+      resolve(false)
+    })
+    socket.on('connect', () => {
+      socket.end()
+      resolve(true)
+    })
+  })
+}
+
+function mpvSeek(pipePath, timestamp) {
+  const socket = net.connect(pipePath)
+  socket.on('connect', () => {
+    console.debug('seek!', timestamp)
+    socket.write(`seek ${timestamp} absolute\n`)
+    socket.end()
+  })
+}
+
+export function isVideoLink(link: string) {
+  return link.startsWith('https://www.bilibili.com/video') || link.startsWith('https://www.youtube.com/watch')
+}
+
+export function isVideoExt(ext: string): boolean {
+  const videoExts = new Set(['mp4', 'webm', 'ogv', 'mov', 'mkv', 'avi', 'flv', 'm3u'])
+  ext = ext.replace(/^\./, '').toLowerCase()
+  return videoExts.has(ext)
+}
+
+export function getRunningMarkdownViewInstance(plugin: Plugin): Promise<MarkdownView> {
+  const { app } = plugin
+  return new Promise((resolve) => {
+    function tryGetMarkdownView() {
+      const views = app.workspace.getLeavesOfType('markdown')
+
+      for (const view of views) {
+        if (view) {
+          const mdView = view.view as MarkdownView
+          console.debug(mdView)
+          if (mdView.editMode) {
+            resolve(mdView)
+            return true
+          }
+        }
+      }
+      return false
+    }
+    app.workspace.onLayoutReady(() => {
+      if (tryGetMarkdownView()) return
+      const onLayoutChange = () => {
+        if (tryGetMarkdownView()) app.workspace.off('layout-change', onLayoutChange)
+      }
+      app.workspace.on('layout-change', onLayoutChange)
+      plugin.register(() => app.workspace.off('layout-change', onLayoutChange))
+    })
+  })
+}
+
+export function getViewPrototype<T>(ctor: T): T {
+  return (ctor as any).prototype
+}
+
+export function getInstanceCtor<T>(instance: T): T {
+  return (instance as any).constructor
+}
+
+export function getInstancePrototype<T>(instance: T): T {
+  return (instance as any).constructor.prototype
+}
+
+declare module 'obsidian' {
+  interface MarkdownPreviewView {
+    rerender(full?: boolean): void
+  }
+  interface App {
+    viewRegistry: ViewRegistry
+    embedRegistry: EmbedRegistry
+  }
+  interface ViewRegistry {
+    typeByExtension: Record<string, string>
+    viewByType: Record<string, ViewCreator>
+    getTypeByExtension(ext: string): string | undefined
+    getViewCreatorByType(type: string): ViewCreator | undefined
+    isExtensionRegistered(ext: string): boolean
+    registerExtensions(exts: string[], type: string): void
+    registerViewWithExtensions(exts: string[], type: string, viewCreator: ViewCreator): void
+    unregisterExtensions(exts: string[]): void
+    unregisterView(viewType: string): void
+  }
+
+  interface EmbedInfo {
+    app: App
+    containerEl: HTMLDivElement
+    depth: number
+    displayMode: boolean
+    linktext: string
+    showInline: boolean
+    sourcePath: string
+  }
+  interface EmbedCreator {
+    (info: EmbedInfo, file: TFile, subpath: string): EmbedComponent
+  }
+  interface EmbedRegistry {
+    embedByExtension: Record<string, EmbedCreator>
+    registerExtension(ext: string, creator: EmbedCreator): void
+    registerExtensions(exts: string[], creator: EmbedCreator): void
+    unregisterExtensions(exts: string[]): void
+    unregisterExtension(ext: string): void
+  }
+  interface EmbedComponent extends Component {
+    loadFile(): any
+  }
+}
+
+export function reloadMarkdownPreview(workspace: Workspace) {
+  workspace.getLeavesOfType('markdown').forEach(async (leaf) => {
+    // (leaf.view as MarkdownView).previewMode?.rerender(true);
+    const state = leaf.getViewState()
+    await leaf.setViewState({ type: 'empty' })
+    await leaf.setViewState(state)
+  })
+}
+
+function getPipePath(link: string) {
+  const b64 = Buffer.from(link).toString('base64')
+  return '\\\\.\\pipe\\' + b64
+}
